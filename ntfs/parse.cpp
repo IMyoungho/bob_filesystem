a3eb080d466d7139ec2067c52c5c161dde832609
root <root@localhost.localdomain>
2018-08-21 07:12:56 +0900

    data attribute runlist finish

--- ntfs/parse.cpp
+++ ntfs/parse.cpp
@@ -7,7 +7,7 @@ parse::parse(int argc, char*argv[]){
 
 void parse::check_argc(int argc, char* argv[]){
     if(argc!=2){
-        cout << " <usage> : <file path> " <<endl;
+        cout << " <Usage> : <File path> " <<endl;
         exit(0);
     }
     this->file_path=argv[1];
@@ -18,7 +18,7 @@ void parse::check_argc(int argc, char* argv[]){
 void parse::file_open_check(){
     this->fp = fopen(this->file_path,"rb");
     if(!this->fp){
-        perror("File Open Error!!\n");
+        perror("File Open Error !!\n");
         exit(0);
     }
     cout << " >> File Open Successed !!" << endl;
@@ -28,8 +28,8 @@ void parse::file_reading(){
     struct ntfs_struct ntfs_h;
     fread(&ntfs_h, 1, sizeof(ntfs_struct),this->fp);
     int jump_offset = static_cast<int>(ntfs_h.start_of_mft * ntfs_h.sector_per_cluster * ntfs_h.byte_per_sector);
+    //cout << "jump_offset = " << jump_offset << endl; //temp
     mft_entry_parsing(jump_offset);
-    cout << "jump_offset = " << jump_offset << endl;
 }
 
 void parse::mft_entry_parsing(int jump_offset){
@@ -39,9 +39,7 @@ void parse::mft_entry_parsing(int jump_offset){
     struct mft_entry_header *mft_entry_h=(struct mft_entry_header*)this->temp;
     this->temp+=mft_entry_h->offset_to_first_attribute;
     struct common_attribute *common_attribute=(struct common_attribute*)(this->temp);
-
     uint8_t *runlist_offset, *runlist_length;   //use data attribute;
-    int offset{0}, length{0};                   //use data attribute;
 
     while(common_attribute->attribute_type_id!=0){
             switch (common_attribute->attribute_type_id){
@@ -50,7 +48,7 @@ void parse::mft_entry_parsing(int jump_offset){
                     struct standard_info_attribute *standard = (struct standard_info_attribute*)this->temp;
                     this->temp+=common_attribute->length_of_attribute;
                     common_attribute=(struct common_attribute*)this->temp;
-                    cout << " [+]Standard Info Attribute Parsing Complete!!" << endl;
+                    cout << " [+]Standard Info Attribute Parsing Complete !!" << endl;
                 }
                 break;
                 case ATTRIBUTE_LIST:
@@ -60,7 +58,7 @@ void parse::mft_entry_parsing(int jump_offset){
                     struct filename_attirbute *filename = (struct filename_attirbute*)this->temp;
                     this->temp+=common_attribute->length_of_attribute;
                     common_attribute=(struct common_attribute*)this->temp;
-                    cout << " [+]File Name Attribute Parsing Complete!!" << endl;
+                    cout << " [+]File Name Attribute Parsing Complete !!" << endl;
                 }
                 break;
                 case VOLUME_VERSION_OBJECT_ID:
@@ -86,21 +84,57 @@ void parse::mft_entry_parsing(int jump_offset){
                         break;
                         case NON_RESIDENT:
                         {
+                            this->jump_box+=jump_runlist; //
                             uint8_t cal_temp{0};   //use data attribute;
                             struct non_resident_header *n_resi_h=(struct non_resident_header*)this->temp;
                             this->temp-=sizeof(struct common_attribute); //Runlist offset 기준이 Data attribute 처음 기준이니까
-                            this->temp+=n_resi_h->offset_to_the_runlist;
-                            this->show_data(this->temp,30);
-                            memcpy(&cal_temp, this->temp,1);
-                            printf("cal_temp = %d", cal_temp);
+                            this->temp+=n_resi_h->offset_to_the_runlist+this->jump_box;//지나온 runlist는 건너뜀
+                            //jump_box를 쓰지않으면 this->temp가 재귀함수호출로 인하여 제대로 jump_runlist만큼 jump할 수 없음
 
+//                            this->show_data(this->temp,30); //temp
+
+                            memcpy(&cal_temp, this->temp,1);
+                            this->offset = cal_temp >> 4;
+                            this->length = cal_temp-((cal_temp >> 4) << 4);
+
+                            runlist_offset = new uint8_t[this->offset];
+                            runlist_length = new uint8_t[this->length];
+
+                            memcpy(runlist_length, this->temp+1, static_cast<size_t>(this->length)); //runlist 바이트를 건너뛰고니까 +1 을 해줌
+                            memcpy(runlist_offset, this->temp+1+this->length, static_cast<size_t>(this->offset)); //runlist 바이트를 건너뛰고니까 +1 을 해줌
+                            memcpy(&this->last_runlist, this->temp+1+this->length+this->offset,1); //last bytes
+                            this->jump_runlist = 1 + this->offset + this->length;
+
+
+//                            show_data(runlist_length, this->length);//temp
+//                            show_data(runlist_offset,this->offset); //temp
+//                            printf("last runlist = %x\n",this->last_runlist);//temp
+//                            printf("jump runlist = %d\n",this->jump_runlist);//temp
+
+
+
+                            if(this->last_runlist==0x00){
+                                delete [] runlist_length;
+                                delete [] runlist_offset;
+                                //cout << " [+]Data Attribute Parsing Complete !!" << endl;
+                                cout << " >> [#]Parsing Finish !!" << endl;
+                                exit(0);
+                            }
+
+                            int jump_offset = 0;                //runlist
+                            jump_offset=runlist_offset[0];
+                            for(int i=0; i < this->offset-1; i++){
+                                jump_offset+=runlist_offset[i+1] << 8*(i+1);  //runlist offset 계산함 배열로 나누어 져있기 때문에 쉬프트연산을 이용하여 맞춰줌
+                            }
+//                            printf("Runlist  = 0x%x\n",jump_offset); //temp
+                            cout << " [!]Jump to runlist offset !! " << endl;
+                            mft_entry_parsing(jump_offset*4096); //끝나지 않았을 경우 재귀하여 다시시작
                         }
                         break;
                         default:
-                            cout << " [-]Error to get resident flag!!" << endl;
+                            cout << " [-]Error to get resident flag !!" << endl;
                         break;
                     }
-                    cout << " [+]Data Attribute Parsing Complete!!" << endl;
                 }
                 break;
                 case INDEX_ROOT:
@@ -131,8 +165,7 @@ void parse::mft_entry_parsing(int jump_offset){
 }
 
 void parse::show_data(uint8_t *data, int length){
-    for(int i=0; i<length; i++)
-    {
+    for(int i=0; i<length; i++){
         if(i%16==0)
             cout << endl;
         printf("%02x ", data[i]);
--- ntfs/parse.h
+++ ntfs/parse.h
@@ -11,15 +11,18 @@ class parse{
 private:
     char * file_path;
     FILE *fp;
-    uint8_t temp_cluster[4096];
-    uint8_t *temp;
+    uint8_t temp_cluster[4096];              //파일을 클러스터단위로 읽어들이기위한 버퍼
+    uint8_t *temp;                           //클러스터배열의 위치 포인터
+    int offset{0}, length{0};                //use data attribute;
+    int jump_runlist{0};                     //use data attribute; 다시 runlist 계산시 건너뛰어야하는 길이
+    uint8_t last_runlist{0};                 //use data attribute; runlist 마지막 바이트
+    int jump_box;                            //jump runlist를 차곡차곡 모아놓음
 public:
     parse(int argc, char *argv[]);
     void check_argc(int argc, char *argv[]);
     void file_open_check();
     void file_reading();
     void mft_entry_parsing(int jump_offset);
-
     void show_data(uint8_t *data, int length);
 };
 
